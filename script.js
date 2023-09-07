3c94b6ca4697e65a950b56f5de284b39697c2d0e
rasmus <rari0001@stud.kea.dk>
2023-09-07 12:12:18 +0200

    script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //    url    //https://tjherkatrrebvelyjaav.supabase.co
 //    api key   //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaGVya2F0cnJlYnZlbHlqYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQyOTAsImV4cCI6MjAwOTY1MDI5MH0.IlwdTgpdK3YPhLi8jKWEXwdYjSFKerx57xii_GoxlfI
+fetch("https://tjherkatrrebvelyjaav.supabase.co/rest/v1/vildtmaddata", {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaGVya2F0cnJlYnZlbHlqYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQyOTAsImV4cCI6MjAwOTY1MDI5MH0.IlwdTgpdK3YPhLi8jKWEXwdYjSFKerx57xii_GoxlfI",
+  },
+})
+  .then((res) => res.json())
+  .then(showData);
+
+function showData(items) {
+  console.log(items);
+  //items.forEach()
+}
