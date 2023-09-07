a5c257c20816227edc2fb73c0752dcd0c53e3af4
rasmus <rari0001@stud.kea.dk>
2023-09-07 12:03:08 +0200

    url og api

---
+++ script.js
@@ -0,0 +1,2 @@
+//    url    //https://tjherkatrrebvelyjaav.supabase.co
+//    api key   //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaGVya2F0cnJlYnZlbHlqYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQyOTAsImV4cCI6MjAwOTY1MDI5MH0.IlwdTgpdK3YPhLi8jKWEXwdYjSFKerx57xii_GoxlfI
