4e109415bd43848175970603ec47cbecb260419b
rasmus <rari0001@stud.kea.dk>
2023-09-11 13:41:48 +0200

    nyeting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //    url    //https://tjherkatrrebvelyjaav.supabase.co
 //    api key   //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaGVya2F0cnJlYnZlbHlqYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQyOTAsImV4cCI6MjAwOTY1MDI5MH0.IlwdTgpdK3YPhLi8jKWEXwdYjSFKerx57xii_GoxlfI
-fetch("https://tjherkatrrebvelyjaav.supabase.co/rest/v1/vildtmaddata", {
+fetch("https://tjherkatrrebvelyjaav.supabase.co/rest/v1/vildtmaddata?select=name,description,image,landscape", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaGVya2F0cnJlYnZlbHlqYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQyOTAsImV4cCI6MjAwOTY1MDI5MH0.IlwdTgpdK3YPhLi8jKWEXwdYjSFKerx57xii_GoxlfI",
@@ -13,3 +13,20 @@ function showData(items) {
   console.log(items);
   //items.forEach()
 }
+//header
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li a");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+  });
+});
