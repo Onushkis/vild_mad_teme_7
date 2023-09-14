1f20065ca30fc7c4639cda3c2b293e1ca6f2071f
AugustS-J <ausc0001@stud.kea.dk>
2023-09-14 20:41:05 +0200

    Noter på JS

--- kort.js
+++ kort.js
@@ -1,21 +1,20 @@
+//laver objektet urlParams, gør det muligt at arbejde med parametre
 const urlParams = new URLSearchParams(window.location.search);
-// console.log(urlParams);
+//fanger parameteret landscape, og gemmer dets værdi i variablen landscape
 const landscape = urlParams.get("landscape");
+//fanger parameteret food, og gemmer dets værdi i variablen food
 const food = urlParams.get("food");
-console.log(landscape);
-let aarstid = landscape;
-console.log(aarstid);
-// console.log(landscape);
-console.log(landscape.landscape);
-
+//sætter variablen raavarer til at have samme værdi som landscape variablen
+let raavarer = landscape;
+//ændrer billede efter food variablens værdi, med betingelse
 if (food == "Pizza") {
   document.querySelector(".Strand_img").src = "img/pizza.svg";
 }
 if (food == "Pasta") {
   document.querySelector(".Strand_img").src = "img/pasta.svg";
 }
-
-fetch(`https://tjherkatrrebvelyjaav.supabase.co/rest/v1/vildtmaddata?landscape=eq.${aarstid}`, {
+// henter data, påvirker landscape query parameteret med raavarer variablen
+fetch(`https://tjherkatrrebvelyjaav.supabase.co/rest/v1/vildtmaddata?landscape=eq.${raavarer}`, {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqaGVya2F0cnJlYnZlbHlqYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQyOTAsImV4cCI6MjAwOTY1MDI5MH0.IlwdTgpdK3YPhLi8jKWEXwdYjSFKerx57xii_GoxlfI",
@@ -24,35 +23,23 @@ fetch(`https://tjherkatrrebvelyjaav.supabase.co/rest/v1/vildtmaddata?landscape=e
   .then((res) => res.json())
   .then(showLandscapes);
 function showLandscapes(landscapes) {
-  console.log(landscapes);
-
+  //tjekker efter html elementet .main-title
   if (document.querySelector(".main_title")) {
+    //loop itererer gennem hvert element
     landscapes.forEach((singleElement) => {
-      //console.log(singleElement.image);
-      //cloner og ændrer indhold
+      //kloner templatens indhold
       const clone = document.querySelector("#template-opskrift").content.cloneNode(true);
+      //sætter src attributten til værdien på image
       clone.querySelector(".img1").setAttribute("src", singleElement.image);
+      //sætter tekst indholdet til værdien på name
       clone.querySelector(".caption").textContent = singleElement.name;
-
+      //appender den klonede template til html elementet .block
       document.querySelector(".block").appendChild(clone);
     });
   }
-  //   //looper og kalder showLandscape
-  //   //->landscapes.forEach(showLandscape);
 }
-console.log();
+//tjekker efter html elementet .pizzaClick
 if (document.querySelector(".pizzaClick")) {
-  document.querySelector(".pizzaClick").setAttribute("href", `opskrifter.html?landscape=${aarstid}`);
+  //sætter href attributten til raavarer variablens url værdi
+  document.querySelector(".pizzaClick").setAttribute("href", `opskrifter.html?landscape=${raavarer}`);
 }
-
-// function showLandscape(landscape) {
-//   console.log(landscape);
-//   //fang template
-//   const template = document.querySelector("#landskab-kort").content;
-//   //lav en kopi
-//   document.querySelector(".headline").textContent = landscape;
-//   const copy = template.cloneNode(true);
-//   //ændre indhold
-//   //appende
-//   document.querySelector("#landskab-kort").appendChild(copy);
-// }
